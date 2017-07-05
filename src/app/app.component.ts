9ce990690c50300b786bacbe1f8452de1255bcba
maindeveloper113 <maindeveloper113@hotmail.com>
2017-07-05 23:41:01 +0800

    hero editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my first angular app';
+  title: string;
+  hero: Hero;
+
+  constructor() {
+    this.title  = 'Tour of Heroes';
+    this.hero = {id:1, name: "Alic"};
+  }
+ 
+  
 }
 
 
